c7ef1079897b4090d9a6fbd4c8bcbfb194e00f0e
Dmitry Groza <boxfrommars@gmail.com>
2016-01-05 23:12:08 +0300

    Refactoring: form control directive

---
+++ src/app/components/control-group/control-group.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular.module('csvToHeatmap')
+    .directive('controlGroup', controlGroup);
+
+  /** @ngInject */
+  function controlGroup() {
+
+    return {
+      restrict: 'E',
+      transclude: true,
+      require: '^form',
+      templateUrl: 'app/components/control-group/control-group.html',
+      scope: {
+        fieldLabel: '@',
+        fieldName: '@',
+        labelWidth: '@'
+      },
+      link: function (scope, el, attrs, formCtrl) {
+        scope.form = formCtrl;
+      }
+    };
+  }
+})();
